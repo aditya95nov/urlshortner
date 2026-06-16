997b262cc3eb03f41dddaeaeb2a7d3efb811a717
Aditya Sharma <aditya95nov@gmail.com>
2026-06-16 16:39:18 +0530

    add validator and application

---
+++ src/main/java/org/service/UrlValidator.java
@@ -0,0 +1,32 @@
+package org.service;
+
+import org.exception.InvalidUrlException;
+import org.springframework.stereotype.Component;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Set;
+
+@Component
+public class UrlValidator {
+
+    public void validate(String url) {
+
+        try {
+            URI uri = new URI(url);
+
+            if (uri.getScheme() == null ||
+                    uri.getHost() == null) {
+                throw new InvalidUrlException();
+            }
+
+            if (!Set.of("http", "https")
+                    .contains(uri.getScheme().toLowerCase())) {
+                throw new InvalidUrlException();
+            }
+
+        } catch (URISyntaxException e) {
+            throw new InvalidUrlException();
+        }
+    }
+}
